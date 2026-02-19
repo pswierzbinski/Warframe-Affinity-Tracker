9820c243641e1c1828e1ed0e1f9104ed38bbad18
Servedren <pawel.swierzbinski22@gmail.com>
2026-02-19 09:03:21 +0100

    updated modules

--- scripts/copyItems.ts
+++ scripts/copyItems.ts
@@ -1,5 +1,9 @@
 import * as fs from 'fs';
 import * as path from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
 
 const src = path.join(__dirname, '../node_modules/@wfcd/items/data/json');
 const dest = path.join(__dirname, '../public/data');
@@ -9,3 +13,4 @@ if (!fs.existsSync(dest)) {
 }
 
 fs.cpSync(src, dest, { recursive: true });
+console.log('Items data copied successfully');
