c528517e3617a5ec70c79639d21ff933d6fa8fa0
Servedren <pawel.swierzbinski22@gmail.com>
2026-02-19 08:48:54 +0100

    Vercel fixes
    removed githubpages paths from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,10 @@
 import type { NextConfig } from "next";
 
-const isProd = process.env.NODE_ENV === 'production';
 const nextConfig: NextConfig = {
   reactStrictMode: true,
   images: {
-    unoptimized: true, // Disable default image optimization
+    unoptimized: true,
   },
-  assetPrefix: isProd ? '/Warframe-Affinity-Tracker/' : '',
-  basePath: isProd ? '/Warframe-Affinity-Tracker' : '',
 };
-
 
 export default nextConfig;
