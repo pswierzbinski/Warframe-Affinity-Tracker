8af0e66b0131a19f6a100e75a9085b1aa35cb26d
Servedren <pawel.swierzbinski22@gmail.com>
2026-03-19 14:19:48 +0100

    Added image skeleton so the page doesnt jump on switching pages

--- src/components/Items/ItemImage.tsx
+++ src/components/Items/ItemImage.tsx
@@ -1,11 +1,17 @@
-import { Image } from "@heroui/react";
+import { Skeleton } from "@heroui/react";
 import React from "react";
 import NextImage from "next/image";
 
 export function ItemImage({ src }: { src: string }) {
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    const toggleLoad = () => {
+    setIsLoaded(!isLoaded);
+  };
   return (
-    <div className="max-w-30 max-h-30 3xl:max-w-40 3xl:max-h-40 4xl:max-w-70 4xl:max-h-70">
-      <NextImage  alt="Item image" src={src} width={320} height={320} className="w-full h-full object-contain"/>
-    </div>
+    <Skeleton className="w-30 h-30 3xl:w-40 3xl:h-40 4xl:w-70 4xl:h-70 bac" isLoaded={isLoaded} disableAnimation={true} classNames={{
+      base: isLoaded ? "opacity-100" : "opacity-0"
+    }}>
+      <NextImage onLoad={event => toggleLoad()} alt="Item image" src={src} width={320} height={320} className="w-full h-full object-contain"/>
+    </Skeleton >
   );
 }
